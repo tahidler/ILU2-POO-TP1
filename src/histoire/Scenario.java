2302eb18f59073e685b9c1ef45e4e8edebd68081
dadlic <dadlicassogbaa@gmail.com>
2025-02-22 16:33:51 +0100

    Mise à jour de Scenario.java

--- src/histoire/Scenario.java
+++ src/histoire/Scenario.java
@@ -7,7 +7,7 @@ import villagegaulois.Etal;
 import villagegaulois.Village;
 
 public class Scenario {
-
+	
 	public static void main(String[] args) {
 		Village village = new Village("le village des irréductibles", 10, 5);
 		Chef abraracourcix = new Chef("Abraracourcix", 10, village);
